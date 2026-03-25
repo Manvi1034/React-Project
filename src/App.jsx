5c76fec0e202d80d23ebc35f86f50329781aecde
Manvi1034 <manvikalal750@gmail.com>
2026-03-25 11:41:48 +0530

    final commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Quiz from './component/Quiz'
+import QuestionList from './component/QuestionList'
+
+function App() {
+
+  return (
+    <div>
+      <Quiz/>
+    </div>
+  )
+}
+
+export default App
